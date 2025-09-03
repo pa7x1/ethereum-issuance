c579cc5cfcb4403b6b9c5683d4a3c329c0b30ef6
pa7x1 <114529266+pa7x1@users.noreply.github.com>
2025-09-03 19:31:19 +0200

    add vitalik's stake capping issuance curve

---
+++ plots/vitaliks_issuance_curve/issuance_plot.py
@@ -0,0 +1,34 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import numpy as np
+from issuance import issuance
+from yield_curves import vitaliks_issuance_yield
+from common import CIRCULATING_SUPPLY
+from plots.formatters import issuance_formatter, stake_formatter
+
+x = np.linspace(3200, CIRCULATING_SUPPLY, 240)
+
+
+issuance_plot = issuance(vitaliks_issuance_yield, x) / CIRCULATING_SUPPLY
+
+
+
+fig = plt.figure(figsize=(10, 6))
+
+plt.plot(x, issuance_plot, color="blue", linewidth=1)
+
+# Add titles and labels
+plt.title("Vitalik's Stake Capping Issuance")
+
+plt.gca().xaxis.set_major_formatter(FuncFormatter(stake_formatter))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(issuance_formatter))
+
+plt.xlabel('Stake (Millions of ETH)')
+plt.ylabel('Issuance (%)')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.axhline(y=0, color='k', linewidth=0.5)
+plt.axvline(x=0, color='k', linewidth=0.5)
+plt.show()
+fig.savefig('./vitalik_issuance_plot.png', dpi=fig.dpi)
--- yield_curves.py
+++ yield_curves.py
@@ -1,6 +1,7 @@
 from typing import Union
 import numpy as np
 import numpy.typing as npt
+from common import CIRCULATING_SUPPLY
 
 
 def ethereum_issuance_yield(staked: Union[float, npt.NDArray[np.float64]]) -> Union[float, npt.NDArray[np.float64]]:
@@ -17,7 +18,7 @@ def ethereum_issuance_with_burn_yield(staked: Union[float, npt.NDArray[np.float6
     :param staked: Amount of ETH staked.
     :return: The annualized nominal yield.
     """
-    return 1. + 2.6 * 64 * (staked ** -0.5) - 2.6 * np.log(staked) / 2048.
+    return 1. + 3. * (2.6 * 64 * (staked ** -0.5) - 2.6 * np.log(staked) / 2048.)
 
 def ethereum_issuance_with_burn_2_yield(staked: Union[float, npt.NDArray[np.float64]]) -> Union[float, npt.NDArray[np.float64]]:
     """
@@ -26,3 +27,30 @@ def ethereum_issuance_with_burn_2_yield(staked: Union[float, npt.NDArray[np.floa
     :return: The annualized nominal yield.
     """
     return 1. + 2.6 * 128 * (staked ** -0.5) - 2.6 * np.log(staked) / 1024.
+
+def tempered_issuance(staked: Union[float, npt.NDArray[np.float64]], k: int = 2**25) -> Union[float, npt.NDArray[np.float64]]:
+    """
+    Tempered issuance yield curve as proposed by A. Elowsson.
+    :param staked: Amount of ETH staked.
+    :param k: The k parameter that determines the stake at which yields peak.
+    :return: The annualized nominal yield.
+    """
+    return 1. + 2.6 * 64 * staked ** -0.5 / (1 + staked / k)
+
+def log_issuance_with_burn_yield(staked: Union[float, npt.NDArray[np.float64]]) -> Union[float, npt.NDArray[np.float64]]:
+    """
+    A proposal for Ethereum's issuance yield curve where a negative 'burn' term is introduced targeting a max stake rate.
+
+    :param staked: Amount of ETH staked.
+    :return: The annualized nominal yield.
+    """
+    return 1. + 2.6 * (np.log(1. - (staked / CIRCULATING_SUPPLY)) - np.log(staked / CIRCULATING_SUPPLY)) / 128.
+
+def vitaliks_issuance_yield(staked: Union[float, npt.NDArray[np.float64]]) -> Union[float, npt.NDArray[np.float64]]:
+    """
+    A proposal by V. Buterin for Ethereum's issuance yield curve where a negative 'burn' term is introduced.
+    Source: https://notes.ethereum.org/@vbuterin/single_slot_finality#Economic-capping-of-total-deposits
+    :param staked: Amount of ETH staked.
+    :return: The annualized nominal yield.
+    """
+    return 1. + 2.6 * 64 * ((staked ** -0.5) - 0.5 * (2 ** 25 - staked) ** -0.5)

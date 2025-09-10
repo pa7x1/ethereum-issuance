133cd5cdd2ddca0696690dc861e9e8b7630e5c81
pa7x1 <114529266+pa7x1@users.noreply.github.com>
2025-09-10 17:58:31 +0200

    update real yield plot

--- plots/ethereum_issuance/real_yield_plot.py
+++ plots/ethereum_issuance/real_yield_plot.py
@@ -1,54 +1,8 @@
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
-import numpy as np
 from yield_curves import ethereum_issuance_yield
-from yields import real_issuance_yield, effective_holding_yield
-from common import CIRCULATING_SUPPLY, percentage_yield
-from cost_structure import home_staking, lst_staking, institutional_staking
-from plots.formatters import issuance_formatter, stake_formatter
+from plots.real_yield_plot import plot
+from common import CIRCULATING_SUPPLY
 
-x = np.linspace(3200, CIRCULATING_SUPPLY, 240)
-
-
-real_staking_yield_solo = percentage_yield(real_issuance_yield(issuance_yield=ethereum_issuance_yield(x),
-                                                               supply=CIRCULATING_SUPPLY,
-                                                               staked=x,
-                                                               **home_staking))
-real_staking_yield_lst = percentage_yield(real_issuance_yield(issuance_yield=ethereum_issuance_yield(x),
-                                                              supply=CIRCULATING_SUPPLY,
-                                                              staked=x,
-                                                              **lst_staking))
-real_staking_yield_institutional = percentage_yield(
-    real_issuance_yield(issuance_yield=ethereum_issuance_yield(x),
-                        supply=CIRCULATING_SUPPLY,
-                        staked=x,
-                        **institutional_staking))
-
-real_holding_yield = percentage_yield(
-    effective_holding_yield(issuance_yield=ethereum_issuance_yield(x), supply=CIRCULATING_SUPPLY, staked=x))
-
-yield_delta = real_staking_yield_lst - real_holding_yield
-
-fig = plt.figure(figsize=(10, 6))
-
-plt.plot(x, real_staking_yield_solo, label="Real Yield Home Validator", color="blue", linewidth=1)
-plt.plot(x, real_staking_yield_lst, label="Real Yield LST", color="green", linewidth=1)
-plt.plot(x, real_staking_yield_institutional, label="Real Yield Institutional", color="orange", linewidth=1)
-plt.plot(x, real_holding_yield, label="Real Holding Yield", color="red", linewidth=1)
-plt.plot(x, yield_delta, label="(LST - Holding) Real Yield Delta", color="purple", linewidth=1, linestyle="--")
-
-# Add titles and labels
-plt.title("Ethereum Real Issuance Yield")
-
-plt.gca().xaxis.set_major_formatter(FuncFormatter(stake_formatter))
-plt.xlabel('Stake (Millions of ETH)')
-plt.ylabel('Yield (%)')
-plt.ylim(top=10, bottom=-3)
-plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-plt.axhline(y=0, color='k', linewidth=0.5)
-plt.axvline(x=0, color='k', linewidth=0.5)
-plt.legend(fontsize=12)
-plt.show()
-fig.savefig('./ethereum_real_yield_plot.png', dpi=fig.dpi)
+plot(ethereum_issuance_yield,
+     title="Ethereum Issuance",
+     circulating_supply=CIRCULATING_SUPPLY,
+     filename="ethereum_real_yield_plot.png")

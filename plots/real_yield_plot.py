275271aa03b3d1d29885bfaf444a556603fdc603
pa7x1 <114529266+pa7x1@users.noreply.github.com>
2025-09-15 20:22:29 +0200

    refactor

---
+++ plots/real_yield_plot.py
@@ -0,0 +1,70 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import numpy as np
+from yields import real_issuance_yield, effective_holding_yield
+from common import CIRCULATING_SUPPLY, percentage_yield
+from plots.formatters import stake_formatter
+from cost_structure import home_staking, lst_staking, institutional_staking
+from typing import Callable
+
+
+def plot(yield_curve: Callable,
+         title: str,
+         circulating_supply: float = CIRCULATING_SUPPLY,
+         filename: str = "real_yield_plot.png") -> None:
+
+    x = np.linspace(3200, circulating_supply * 0.99, 240)
+
+
+    real_staking_yield_solo = percentage_yield(
+        real_issuance_yield(
+            issuance_yield=yield_curve(x),
+            supply=circulating_supply,
+            staked=x,
+            **home_staking))
+    real_staking_yield_lst = percentage_yield(
+        real_issuance_yield(
+            issuance_yield=yield_curve(x),
+            supply=circulating_supply,
+            staked=x,
+            **lst_staking))
+    real_staking_yield_institutional = percentage_yield(
+        real_issuance_yield(
+            issuance_yield=yield_curve(x),
+            supply=circulating_supply,
+            staked=x,
+            **institutional_staking))
+
+    real_holding_yield = percentage_yield(
+        effective_holding_yield(
+            issuance_yield=yield_curve(x),
+            supply=circulating_supply,
+            staked=x))
+
+    yield_delta = real_staking_yield_lst - real_holding_yield
+
+
+    fig = plt.figure(figsize=(10, 6))
+
+    plt.plot(x, real_staking_yield_solo, label="Issuance Real Yield Home Validator", color="blue", linewidth=1)
+    plt.plot(x, real_staking_yield_lst, label="Issuance Real Yield LST", color="green", linewidth=1)
+    plt.plot(x, real_staking_yield_institutional, label="Issuance Real Yield Institutional", color="orange", linewidth=1)
+    plt.plot(x, real_holding_yield, label="Issuance Real Holding Yield", color="red", linewidth=1)
+    plt.plot(x, yield_delta, label="(LST - Holding) Issuance Real Yield Delta", color="purple", linewidth=1, linestyle="--")
+
+    # Add titles and labels
+    plt.title(title)
+
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(stake_formatter))
+    plt.xlabel('Stake (Millions of ETH)')
+    plt.ylabel('Yield (%)')
+    plt.ylim(top=10, bottom=-3)
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+    plt.axhline(y=0, color='k', linewidth=0.5)
+    plt.axvline(x=0, color='k', linewidth=0.5)
+    plt.legend(fontsize=12)
+    plt.show()
+    fig.savefig(f'./{filename}', dpi=fig.dpi)
+    return
